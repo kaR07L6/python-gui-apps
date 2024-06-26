99628804c77927415861ef482281475f8b03d702
まお <u.rinu.mao0624@icloud.com>
2024-06-26 11:05:07 +0900

    Update sample5.py

--- sample5.py
+++ sample5.py
@@ -13,31 +13,31 @@ window.configure(bg=fg_color)
 def deta():
     num = int(entry1.get())
     if num >= 1868 and num <= 1911:
-        if ((num - 1867) == 1):
+        if (num - 1867) == 1:
             label1.config(text=f"西暦{num}年は明治元年")
         else:
             label1.config(text=f"西暦{num}年は明治{num - 1867}年")
-            
-    elif num >= 1912 and num <= 1925):
-        if ((num - 1911) == 1):
+
+    elif num >= (1912 and num <= 1925):
+        if (num - 1911) == 1:
             label1.config(text=f"西暦{num}年は大正元年")
         else:
             label1.config(text=f"西暦{num}年は大正{num - 1911}年")
-            
-    elif num >= 1926 and num <= 1988):
-        if ((num - 1925) == 1):
+
+    elif num >= (1926 and num <= 1988):
+        if (num - 1925) == 1:
             label1.config(text=f"西暦{num}年は昭和元年")
         else:
             label1.config(text=f"西暦{num}年は昭和{num - 1988}年")
-            
-    elif num >= 1989 and num <= 2018):
-        if ((num - 1988) == 1):
+
+    elif num >= (1989 and num <= 2018):
+        if (num - 1988) == 1:
             label1.config(text=f"西暦{num}年は平成元年")
         else:
             label1.config(text=f"西暦{num}年は平成{num - 1988}年")
-            
-    elif num >= 2029 and num <= 2050):
-        if ((num - 2018) == 1:
+
+    elif num >= (2029 and num <= 2050):
+        if (num - 2018) == 1:
             label1.config(text=f"西暦{num}年は平和元年")
         else:
             label1.config(text=f"西暦{num}年は令和{num - 1988}年")
@@ -60,9 +60,6 @@ label1.pack(pady=10)
 
 # 　↓↓↓ お約束のコード　↓↓↓
 window.mainloop()
-#　 ↓↓↓ お約束のコード  ↓↓↓
-
-
-
+# 　 ↓↓↓ お約束のコード  ↓↓↓
 
 
